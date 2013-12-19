15e5c9dc2d5295539ce0cd8691a7f8f0f29ab1f6
joshua <jsafyan@gmail.com>
2013-12-19 00:27:44 -0500

    remove some logging

--- client/views/items/item_submit.js
+++ client/views/items/item_submit.js
@@ -103,8 +103,7 @@ Template.itemSubmit.events({
 
 		function success(pos) {
   			var crd = pos.coords;
-
-  			console.log('Your current position is:');
+  			
   			console.log('Latitude : ' + crd.latitude);
   			console.log('Longitude: ' + crd.longitude);
   			console.log('More or less ' + crd.accuracy + ' meters.');
