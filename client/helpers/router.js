fe51356f0d60a042484a86fbbbdf41925d50622c
joshua <jsafyan@gmail.com>
2014-02-21 02:15:28 -0500

    added accounts-entry, some header and profile dashboard cleanup

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -5,15 +5,17 @@ Router.map(function() {
 		waitOn: function() {
 			return [Meteor.subscribe('items'), Meteor.subscribe('photos')];
 		}
-		});
+	});
 	this.route('itemPage', {
 		path: '/items/:_id',
 		data: function() {
 			return Items.findOne(this.params._id);
-		}}),
+		}
+	});
 	this.route('itemSubmit', {
 		path: '/submit',
 		before: function() {
+			AccountsEntry.signInRequired(this);
 			this.subscribe('photos').wait();
 		}
 	});
@@ -24,22 +26,28 @@ Router.map(function() {
 		},
 		waitOn: function() {
 			return [Meteor.subscribe('items'), Meteor.subscribe('photos')];
+		},
+		before: function() {
+			AccountsEntry.signInRequired(this);
 		}
 	});
 	this.route('userProfile', {
-		path: '/profile/',
+		path: '/dashboard/',
 		data: function() {
 			return Meteor.users.findOne(Meteor.user());
+		},
+		before: function() {
+			AccountsEntry.signInRequired(this);
 		}
 	});
 });
 
-// Restrict access to certain pages to logged in users
+// Pre-loading hooks
 Router.configure({
 	before: function() {
 		// clear any visible error messages
 		Errors.clearSeen();
-
+		/*
 		var routeName = this.route.name;
 
 		// no need to check whether logged in at these URLs
@@ -53,5 +61,6 @@ Router.configure({
 			this.render(Meteor.loggingIn() ? 'loading' : 'accessDenied');
 			return this.stop();
 		}
+		*/
 	}
 });
